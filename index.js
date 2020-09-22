2dcdf2ef1a3d02581da0c17053b6a6efa5ac11f7
Eric Zanchettin Presto <canalmontrisz@hotmail.com>
2020-09-21 22:02:54 -0300

    chore: Altera o retorno da Rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/teste', (res, req) => {
-    return res.send('Hello World!');
+    return res.send('Hello World!²');
 });
 
 app.listen(3333);
